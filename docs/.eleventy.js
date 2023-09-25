c43c51fa2a4e759a490b8425364cc6ac19035ac6
Alexander Somma <admin@openco.ca>
2023-09-25 09:56:13 +0000

    nunjuk templates & optimized all lighthouse scores

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,3 +1,17 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({ static: "/"  });
+module.exports = function(eleventyConfig) {
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
 };
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ static: "/"  })
+  return {
+    markdownTemplateEngine: "njk"
+  };
+};
+
